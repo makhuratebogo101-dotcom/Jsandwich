068093ce14ad2f8222d02794b6aec34959a13fc4
makhuratebogo-dotcom <makhuratebogo101@gmail.com>
2026-05-13 15:32:12 +0200

    Full project done

---
+++ JSandwich.java/src/main/java/com/mycompany/jsandwich/java/JSandwichJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jsandwich.java;
+
+/**
+ *
+ * @author 25055139
+ */
+public class JSandwichJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new NewJFrame (). setVisible (true);
+    } 
+}
+
